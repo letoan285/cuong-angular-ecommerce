2ec1d5a15dc40cc76af334c08af3b7ad92398362
Le Cuong <lecuong0704@gmail.com>
2019-07-01 21:48:49 +0700

    update

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -19,17 +19,16 @@ export class ProductComponent implements OnInit {
   });
   public Total: number;
   public t: any = [];
-  getBigTotal() {
-    const reducer = (a, b) => a + b;
-    return this.cart.map(el => el = el.total).reduce(reducer);
-  }
   constructor() {
     this.cart = data.rootCarts;
   }
 
   ngOnInit() {
   }
-
+  getBigTotal() {
+    const reducer = (a, b) => a + b;
+    document.getElementById('total').innerHTML = this.cart.map(el => el = el.total).reduce(reducer);
+  }
   addToCart(x: number): any {
     const result = this.items.find(item => item.id === x);
     const cart = this.cart;
@@ -39,17 +38,20 @@ export class ProductComponent implements OnInit {
       result.buy++;
       result.total = result.buy * result.price;
       cart.push(result);
+      this.getBigTotal();
     } else if (result.stock >= 1) {
       if ( this.t.filter( el => result.id === el ).length > 0 ) {
         result.stock--;
         result.buy++;
         result.total = result.buy * result.price;
+        this.getBigTotal();
       } else {
         result.buy++;
         result.stock--;
-        this.t.push(result.id);
         result.total = result.buy * result.price;
+        this.t.push(result.id);
         cart.push(result);
+        this.getBigTotal();
         }
     } else {
       alert('Sản phẩm đã hết');
